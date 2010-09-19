6117777c10cf6b9559ddbf450d78dfacdb013424
JHoxley_cp <SND\JHoxley_cp>
2010-09-19 08:01:24 -0700

    Added stub project for Direct3D 11 Tessellation Parameters Demo

---
+++ trunk/Hieroglyph3/Applications/TessellationParams/App.cpp
@@ -0,0 +1,227 @@
+//--------------------------------------------------------------------------------
+#include "App.h"
+#include "Log.h"
+
+#include <sstream>
+
+#include "EventManager.h"
+#include "EvtFrameStart.h"
+#include "EvtChar.h"
+#include "EvtKeyUp.h"
+#include "EvtKeyDown.h"
+
+#include "ScriptManager.h"
+
+#include "SwapChainConfigDX11.h"
+#include "Texture2dConfigDX11.h"
+
+using namespace Glyph3;
+//--------------------------------------------------------------------------------
+App AppInstance; // Provides an instance of the application
+//--------------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------------
+App::App()
+{
+	m_bSaveScreenshot = false;
+}
+//--------------------------------------------------------------------------------
+bool App::ConfigureEngineComponents()
+{
+	// The application currently supplies the 
+	int width = 320;
+	int height = 240;
+	bool windowed = true;
+
+	// Set the render window parameters and initialize the window
+	m_pWindow = new Win32RenderWindow();
+	m_pWindow->SetPosition( 25, 25 );
+	m_pWindow->SetSize( width, height );
+	m_pWindow->SetCaption( std::wstring( L"Direct3D 11 Tessellation Parameters Demo" ) );
+	m_pWindow->Initialize();
+
+	
+	// Create the renderer and initialize it for the desired device
+	// type and feature level.
+
+	m_pRenderer11 = new RendererDX11();
+
+	if ( !m_pRenderer11->Initialize( D3D_DRIVER_TYPE_HARDWARE, D3D_FEATURE_LEVEL_10_0 ) )
+	{
+		Log::Get().Write( L"Could not create hardware device, trying to create the reference device..." );
+
+		if ( !m_pRenderer11->Initialize( D3D_DRIVER_TYPE_REFERENCE, D3D_FEATURE_LEVEL_10_0 ) )
+		{
+			ShowWindow( m_pWindow->GetHandle(), SW_HIDE );
+			MessageBox( m_pWindow->GetHandle(), L"Could not create a hardware or software Direct3D 11 device - the program will now abort!", L"Hieroglyph 3 Rendering", MB_ICONEXCLAMATION | MB_SYSTEMMODAL );
+			RequestTermination();			
+			return( false );
+		}
+
+		// If using the reference device, utilize a fixed time step for any animations.
+		m_pTimer->SetFixedTimeStep( 1.0f / 10.0f );
+	}
+
+
+	// Create a swap chain for the window that we started out with.  This
+	// demonstrates using a configuration object for fast and concise object
+	// creation.
+
+	SwapChainConfigDX11 Config;
+	Config.SetWidth( m_pWindow->GetWidth() );
+	Config.SetHeight( m_pWindow->GetHeight() );
+	Config.SetOutputWindow( m_pWindow->GetHandle() );
+	m_iSwapChain = m_pRenderer11->CreateSwapChain( &Config );
+	m_pWindow->SetSwapChain( m_iSwapChain );
+
+	// We'll keep a copy of the render target index to use in later examples.
+
+	m_RenderTarget = m_pRenderer11->GetSwapChainResource( m_iSwapChain );
+
+	// Next we create a depth buffer for use in the traditional rendering
+	// pipeline.
+
+	Texture2dConfigDX11 DepthConfig;
+	DepthConfig.SetDepthBuffer( width, height );
+	m_DepthTarget = m_pRenderer11->CreateTexture2D( &DepthConfig, 0 );
+	
+	// Bind the swap chain render target and the depth buffer for use in 
+	// rendering.  
+
+	m_pRenderer11->m_pPipeMgr->ClearRenderTargets();
+	m_pRenderer11->m_pPipeMgr->BindRenderTargets( 0, m_RenderTarget );
+	m_pRenderer11->m_pPipeMgr->BindDepthTarget( m_DepthTarget );
+	m_pRenderer11->m_pPipeMgr->ApplyRenderTargets();
+
+
+	// Create a view port to use on the scene.  This basically selects the 
+	// entire floating point area of the render target.
+
+	D3D11_VIEWPORT viewport;
+	viewport.Width = static_cast< float >( width );
+	viewport.Height = static_cast< float >( height );
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+
+	int ViewPort = m_pRenderer11->CreateViewPort( viewport );
+	m_pRenderer11->m_pPipeMgr->SetViewPort( ViewPort );
+	
+	return( true );
+}
+//--------------------------------------------------------------------------------
+void App::ShutdownEngineComponents()
+{
+	if ( m_pRenderer11 )
+	{
+		m_pRenderer11->Shutdown();
+		delete m_pRenderer11;
+	}
+
+	if ( m_pWindow )
+	{
+		m_pWindow->Shutdown();
+		delete m_pWindow;
+	}
+}
+//--------------------------------------------------------------------------------
+void App::Initialize()
+{
+	// Basic event handling is supported with the EventManager class.  This is a 
+	// singleton class that allows an EventListener to register which events it
+	// wants to receive.
+
+	EventManager* pEventManager = EventManager::Get( );
+
+	// The application object wants to know about these three events, so it 
+	// registers itself with the appropriate event IDs.
+
+	pEventManager->AddEventListener( SYSTEM_KEYBOARD_KEYUP, this );
+	pEventManager->AddEventListener( SYSTEM_KEYBOARD_KEYDOWN, this );
+	pEventManager->AddEventListener( SYSTEM_KEYBOARD_CHAR, this );
+}
+//--------------------------------------------------------------------------------
+void App::Update()
+{
+	// Update the timer to determine the elapsed time since last frame.  This can 
+	// then used for animation during the frame.
+
+	m_pTimer->Update();
+
+	// Send an event to everyone that a new frame has started.  This will be used
+	// in later examples for using the material system with render views.
+
+	EventManager::Get()->ProcessEvent( new EvtFrameStart() );
+
+	// Clear the window to a time varying color.
+
+	float fBlue = sinf( m_pTimer->Runtime() * m_pTimer->Runtime() ) * 0.25f + 0.5f;
+
+	m_pRenderer11->m_pPipeMgr->ClearBuffers( Vector4f( 0.0f, 0.0f, fBlue, 0.0f ), 1.0f );
+	m_pRenderer11->Present( m_pWindow->GetHandle(), m_pWindow->GetSwapChain() );
+
+	// Save a screenshot if desired.  This is done by pressing the 's' key, which
+	// demonstrates how an event is sent and handled by an event listener (which
+	// in this case is the application object itself).
+
+	if ( m_bSaveScreenshot  )
+	{
+		m_bSaveScreenshot = false;
+		m_pRenderer11->m_pPipeMgr->SaveTextureScreenShot( 0, std::wstring( L"TessParamsDemo_" ), D3DX11_IFF_BMP );
+	}
+}
+//--------------------------------------------------------------------------------
+void App::Shutdown()
+{
+	// Print the framerate out for the log before shutting down.
+
+	std::wstringstream out;
+	out << L"Max FPS: " << m_pTimer->MaxFramerate();
+	Log::Get().Write( out.str() );
+}
+//--------------------------------------------------------------------------------
+bool App::HandleEvent( IEvent* pEvent )
+{
+	eEVENT e = pEvent->GetEventType();
+
+	if ( e == SYSTEM_KEYBOARD_KEYDOWN )
+	{
+		EvtKeyDown* pKeyDown = (EvtKeyDown*)pEvent;
+
+		unsigned int key = pKeyDown->GetCharacterCode();
+
+		return( true );
+	}
+	else if ( e == SYSTEM_KEYBOARD_KEYUP )
+	{
+		EvtKeyUp* pKeyUp = (EvtKeyUp*)pEvent;
+
+		unsigned int key = pKeyUp->GetCharacterCode();
+
+		if ( key == VK_ESCAPE ) // 'Esc' Key - Exit the application
+		{
+			this->RequestTermination();
+			return( true );
+		}
+		else if ( key == 0x53 ) // 'S' Key - Save a screen shot for the next frame
+		{
+			m_bSaveScreenshot = true;
+			return( true );
+		}
+		else
+		{
+			return( false );
+		}
+	}
+
+	
+	return( false );
+}
+//--------------------------------------------------------------------------------
+std::wstring App::GetName( )
+{
+	return( std::wstring( L"Direct3D 11 Tessellation Parameters Demo" ) );
+}
+//--------------------------------------------------------------------------------
---
+++ trunk/Hieroglyph3/Applications/TessellationParams/App.h
@@ -0,0 +1,37 @@
+
+#include "Application.h"
+#include "IEventListener.h"
+
+#include "Win32RenderWindow.h"
+#include "RendererDX11.h"
+
+using namespace Glyph3;
+
+class App : public Application, public IEventListener
+{
+
+public:
+	App();
+	
+public:
+	virtual void Initialize();
+	virtual void Update();
+	virtual void Shutdown();
+
+	virtual bool ConfigureEngineComponents();
+	virtual void ShutdownEngineComponents();
+
+	virtual bool HandleEvent( IEvent* pEvent );
+	virtual std::wstring GetName( );
+
+protected:
+
+	RendererDX11*			m_pRenderer11;
+	Win32RenderWindow*		m_pWindow;
+	
+	int						m_iSwapChain;
+	ResourcePtr				m_RenderTarget;
+	ResourcePtr				m_DepthTarget;
+
+	bool					m_bSaveScreenshot;
+};
