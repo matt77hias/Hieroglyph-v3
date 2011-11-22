0033a9dfdb52f95078b52bc43493e7cbf7b7d1fd
jzink_cp <SND\jzink_cp>
2011-11-21 22:09:42 -0800

    - Added the ViewLightPrepassRenderer files to the LightPrepass project (they were missing on the last commit...).  Thanks to Sephirothusi for pointing it out!

---
+++ trunk/Hieroglyph3/Applications/LightPrepass/ViewLightPrepassRenderer.h
@@ -0,0 +1,88 @@
+//--------------------------------------------------------------------------------
+// This file is a portion of the Hieroglyph 3 Rendering Engine.  It is distributed
+// under the MIT License, available in the root of this distribution and 
+// at the following URL:
+//
+// http://www.opensource.org/licenses/mit-license.php
+//
+// Copyright (c) 2003-2010 Jason Zink 
+//--------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------------
+// ViewLightPrepassRenderer
+//
+// This class provides a subclass of the ViewPerspective render view, which
+// implements a deferred rendering set of operations.  This is accomplished with
+// the help of two sub-render views: ViewGBuffer and ViewLights.  ViewGBuffer is
+// used to produce an appropriate set of geometry buffers, while ViewLights is 
+// used to generate a render target with geometric representations of lights
+// combined with the geometry buffer.
+//
+// To the application, this entire system appears only as a single perspective
+// render view.  This makes the rendering system more modular and easy to use in
+// the end application.
+//--------------------------------------------------------------------------------
+#ifndef ViewLightPrepassRenderer_h
+#define ViewLightPrepassRenderer_h
+//--------------------------------------------------------------------------------
+#include "IRenderView.h"
+#include "ViewGBuffer.h"
+#include "ViewLights.h"
+#include "ViewFinalPass.h"
+#include "SpriteRendererDX11.h"
+#include "SpriteFontDX11.h"
+//--------------------------------------------------------------------------------
+namespace Glyph3
+{
+	class Entity3D;
+
+	class ViewLightPrepassRenderer : public IRenderView
+	{
+	public:
+		ViewLightPrepassRenderer( RendererDX11& Renderer, ResourcePtr RenderTarget );
+
+		virtual void Update( float fTime );
+		virtual void PreDraw( RendererDX11* pRenderer );
+		virtual void Draw( PipelineManagerDX11* pPipelineManager, IParameterManager* pParamManager );
+
+		virtual void SetEntity( Entity3D* pEntity );
+		virtual void SetRoot( Node3D* pRoot );
+
+		virtual void SetRenderParams( IParameterManager* pParamManager );
+		virtual void SetUsageParams( IParameterManager* pParamManager );
+
+		virtual void SetViewMatrix( const Matrix4f& matrix );
+		virtual void SetProjMatrix( const Matrix4f& matrix );
+		
+		void SetClipPlanes( float NearClip, float FarClip );
+		void SetupLights();
+
+		virtual ~ViewLightPrepassRenderer();
+
+	protected:
+
+		int						ResolutionX;
+		int						ResolutionY;
+
+        float					m_fNearClip;
+        float					m_fFarClip;
+
+		ResourcePtr     		m_GBufferTarget;
+		ResourcePtr             m_LightTarget;
+		ResourcePtr				m_DepthTarget;
+		ResourcePtr             m_ReadOnlyDepthTarget;
+		ResourcePtr             m_FinalTarget;
+
+		ResourcePtr             m_ResolveTarget;
+		ResourcePtr				m_BackBuffer;
+
+		ViewGBuffer*			m_pGBufferView;
+		ViewLights*             m_pLightsView;
+		ViewFinalPass*          m_pFinalPassView;
+
+		SpriteRendererDX11		m_SpriteRenderer;
+		SpriteFontDX11			m_Font;
+	};
+};
+//--------------------------------------------------------------------------------
+#endif // ViewLightPrepassRenderer_h
